25e9c45950770ce3c0cfc4c91a35872aed47a7c6
yetzerdev <yybanegas@uth.hn>
2025-11-25 21:32:33 -0600

    Init: Proyecto base Spring Boot

---
+++ src/main/java/com/inventario/inventario_web/InventarioWebApplication.java
@@ -0,0 +1,13 @@
+package com.inventario.inventario_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventario/inventario_web/InventarioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventario.inventario_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
