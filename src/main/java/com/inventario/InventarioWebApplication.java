500ce7f591cc60ea23c2f861370f3441454789bc
yetzerdev <yybanegas@uth.hn>
2025-11-25 23:03:44 -0600

    save a InventarioWebApplication.java file

--- src/main/java/com/inventario/InventarioWebApplication.java
+++ src/main/java/com/inventario/InventarioWebApplication.java
@@ -1,4 +1,4 @@
-package com.inventario.inventario_web;
+package com.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
